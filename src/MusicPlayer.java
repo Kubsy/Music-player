6b32743d622b3dc1d26eeaea05e13c5021331c8f
Jakub Bilinski <80340234+Kubsy@users.noreply.github.com>
2023-05-02 18:36:07 +0100

    Progress with state machine

--- src/MusicPlayer.java
+++ src/MusicPlayer.java
@@ -2,14 +2,15 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.*;
 
-enum state {READY, OPENED, PLAYING, PAUSED, EXIT};
+enum states {READY, OPENED, PLAYING, PAUSED, EXIT};
 
 public class MusicPlayer extends JFrame implements ActionListener
 {
-    private enum actions {OPEN,PLAY,PAUSE,STOP,CLOSE,EXIT};
+    private final byte OPEN = 0, PLAY = 1, PAUSE = 2, STOP = 3, CLOSE = 4, EXIT = 5;
     private final String[] BUTTON_LABELS = {"Open", "Play", "Pause", "Stop", "Close", "Exit"};
     private JButton[] actButtons;
     private JPanel p;
+    private states state = states.READY;
     private JLabel Display;
     private final int MAXWINDOWROW = 2;
     public MusicPlayer()
@@ -29,6 +30,11 @@ public class MusicPlayer extends JFrame implements ActionListener
         {
             actButtons[i] = setupButton(""+BUTTON_LABELS[i], this, p);
         }
+
+        Display.setText("Ready");
+        SetAllButtonsEnabled(false);
+        actButtons[OPEN].setEnabled(true);
+        actButtons[EXIT].setEnabled(true);
     }
 
     private JComponent setupComponent(JComponent comp, Container c)
@@ -46,10 +52,105 @@ public class MusicPlayer extends JFrame implements ActionListener
         return (JButton) setupComponent(button, c);
     }
 
-    //TODO add action for the buttons.
+    private void SetAllButtonsEnabled(boolean b)
+    {
+        for (JButton button : actButtons)
+        {
+            button.setEnabled(b);
+        }
+    }
+
+    private void DoOpened()
+    {
+        Display.setText("Opened");
+        SetAllButtonsEnabled(false);
+        actButtons[PLAY].setEnabled(true);
+        actButtons[EXIT].setEnabled(true);
+        actButtons[CLOSE].setEnabled(true);
+    }
+
+    private void DoPlaying()
+    {
+        Display.setText("Playing");
+        SetAllButtonsEnabled(false);
+        actButtons[PAUSE].setEnabled(true);
+        actButtons[STOP].setEnabled(true);
+        actButtons[EXIT].setEnabled(true);
+    }
+
+    private void DoPaused()
+    {
+        Display.setText("Paused");
+        SetAllButtonsEnabled(false);
+        actButtons[PLAY].setEnabled(true);
+        actButtons[EXIT].setEnabled(true);
+        actButtons[STOP].setEnabled(true);
+    }
+
     @Override
     public void actionPerformed(ActionEvent e)
     {
-
+        switch(state)
+        {
+        case READY:
+            if (e.getSource() == actButtons[OPEN])
+            {
+                DoOpened();
+                state = states.OPENED;
+            }
+            else if (e.getSource() == actButtons[EXIT])
+            {
+                Display.setText("Exited");
+            }
+        case OPENED:
+            if (e.getSource() == actButtons[PLAY])
+            {
+                DoPlaying();
+                state = states.PLAYING;
+            }
+            else if (e.getSource() == actButtons[CLOSE])
+            {
+                //TODO: put this into a function
+                Display.setText("Ready");
+                SetAllButtonsEnabled(false);
+                actButtons[OPEN].setEnabled(true);
+                actButtons[EXIT].setEnabled(true);
+                state = states.READY;
+            }
+            else if (e.getSource() == actButtons[EXIT])
+            {
+                Display.setText("Exited");
+            }
+        case PLAYING:
+            if (e.getSource() == actButtons[PAUSE])
+            {
+                DoPaused();
+                state = states.PAUSED;
+            }
+            else if (e.getSource() == actButtons[STOP])
+            {
+                DoOpened();
+                state = states.OPENED;
+            }
+            else if (e.getSource() == actButtons[EXIT])
+            {
+                Display.setText("Exited");
+            }
+        case PAUSED:
+            if (e.getSource() == actButtons[PLAY])
+            {
+                DoPlaying();
+                state = states.PLAYING;
+            }
+            else if (e.getSource() == actButtons[STOP])
+            {
+                DoOpened();
+                state = states.OPENED;
+            }
+            else if (e.getSource() == actButtons[EXIT])
+            {
+                Display.setText("Exited");
+            }
+        }
     }
 }
